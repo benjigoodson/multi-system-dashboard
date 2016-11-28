6cb6608a932d2a28876628cd3668d6213dfe7393
benjigoodson <benjig100@msn.com>
2016-11-28 16:17:01 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+// Using server.js as our main file
+
+// Import modules
+var express = require('express');
+var mongoose = require('mongoose');
+var fileUpload = require('express-fileupload');
+
+var app = express();
+
+// Setup config
+app.use(fileUpload());
+
+// database config values
+var db = require('./app/config/db');
+
+// select port
+var port = process.env.PORT || 3000;
+
+// register routes
+require('./app/routes')(app);
+
+// connect to our Mongo database
+mongoose.connect(db.uri, db.options);
+
+mongoose.Promise = global.Promise;
+
+// location of web files
+app.use(express.static('public'));
+
+// start listening on the port, display message once complete
+app.listen(port, function () {
+  console.log('Server listening on port ' + port + '...');
+});
+
+// expose app
+exports = module.exports = app;
