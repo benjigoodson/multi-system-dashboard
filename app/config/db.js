271e62885b671a1b89c127289e6043867e44c008
Benjamin Goodson <Benjamin Goodson>
2016-11-29 17:18:27 +0000

    fixes for OpenShift

--- app/config/db.js
+++ app/config/db.js
@@ -1,10 +1,11 @@
 // database config details
 
-var host = "172.30.25.87"; // "127.0.0.1";
+var host = "127.0.0.1";
+
 var port = "27017";
 var database =  "sampledb"; // "dashboardapp";
-var username = "userA3F";
-var password = "Mcsv3q0yWuIDuUmX";
+var username = "";
+var password = "";
 
 module.exports = {
 
--- server.js
+++ server.js
@@ -25,12 +25,12 @@ mongoose.Promise = global.Promise;
 // location of web files
 app.use(express.static('public'));
 
-// select port
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+// Select port and IP
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
  
 app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+  console.log( "Listening on " + server_ip + ", port " + server_port )
 });
 
 // LOCAL SETTINGS
