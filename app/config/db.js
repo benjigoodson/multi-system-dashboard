021981c2b2cd268778d2d44daf89f9e12be1147f
Benjamin Goodson <Benjamin Goodson>
2016-11-29 12:53:04 +0000

    OpenShift express settings update

--- app/config/db.js
+++ app/config/db.js
@@ -1,6 +1,6 @@
 // database config details
 
-var host = "172.30.25.87";
+var host = "172.30.25.87"; // "127.0.0.1";
 var port = "27017";
 var database =  "sampledb"; // "dashboardapp";
 var username = "userA3F";
--- server.js
+++ server.js
@@ -13,9 +13,6 @@ app.use(fileUpload());
 // database config values
 var db = require('./app/config/db');
 
-// select port
-var port = process.env.PORT || 3000;
-
 // register routes
 require('./app/routes')(app);
 
@@ -28,10 +25,21 @@ mongoose.Promise = global.Promise;
 // location of web files
 app.use(express.static('public'));
 
-// start listening on the port, display message once complete
-app.listen(port, function () {
-  console.log('Server listening on port ' + port + '...');
+// select port
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
+
+// LOCAL SETTINGS
+// var server_port = process.env.PORT || 3000;
+
+// start listening on the port, display message once complete
+// app.listen(server_port, server_ip_address, function () {
+  // console.log('Server listening on port ' + port + '...');
+// });
 
 // expose app
 exports = module.exports = app;
