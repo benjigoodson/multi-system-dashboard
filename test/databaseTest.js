ec733dffa098260f6d652eaf4a65711e01f291a7
Benjamin Goodson <Benjamin Goodson>
2017-03-28 11:43:43 +0100

    Added UnitTest library

---
+++ test/databaseTest.js
@@ -0,0 +1,16 @@
+var sinon = require('sinon');
+var chai = require('chai');
+var expect = chai.expect;
+
+// describe('CartSummary', function() {
+//   it('getSubtotal() should return 0 if no items are passed in', function() {
+//     var cartSummary = new CartSummary([]);
+//     expect(cartSummary.getSubtotal()).to.equal(0);
+//   });
+// });
+
+describe('JavaScript example test', function(){
+    it('should return true in JavaScript',function(){
+        expect(true).equal(true);
+    });
+});
