2327b942323b259b0458e6457cf01404fee16de2
Benjamin Goodson <Benjamin Goodson>
2017-04-04 11:36:48 +0100

    Added comments to the dashboard route

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -13,13 +13,17 @@ var router = express.Router();
 router.route('/basic').get(function(req, res) {
     console.log("Requested: GET - /api/dashboard/basic");
 
+    // Call controller method to get all the dashboards basic details
     controller.getAllBasic(function(err, basicDashboards) {
 
         if(err) {
+            // If there is an error thrown log the error
             console.log("Error: " + err);
+            // Return an error to the user
             res.status(500).json({success:false, message: err});
             return;
         } else {
+            // Send the reults of the query
             res.send({success:true, message: "Dashboards loaded.", data : basicDashboards});
         }
 
@@ -31,17 +35,23 @@ router.route('/basic').get(function(req, res) {
 router.route('/').post(function(req, res) {
     console.log("Requested: POST - /api/dashboard");
 
+    // If there is a request body 
     if(req._body) {
 
+        // store the dashboard object 
         var dashboard = req.body;
 
+        // Use the controller's function to create a dashboard and return the created object
         controller.create(dashboard, function(err, newDashboard) {
 
             if(err) {
+                // If there is an error thrown log the error
                 console.log("Error: " + err);
+                // Return an error to the user
                 res.status(500).json({success:false, message: err});
                 return;
             } else {
+                // return the result to the user
                 res.json({success:true, message: "Dashboard created.", data : newDashboard});
             }
 
@@ -49,18 +59,23 @@ router.route('/').post(function(req, res) {
     }
 });
 
+// get a unqiue dashboard
 router.route('/:dashboard_id')
 
     // Get a unique dashboard
     .get(function(req, res) {
 
+        // Get the id from the paremters passed
         var id = req.params.dashboard_id;
+
         console.log("Requested: GET - /api/dashboard/" + id);
 
         // Get dashboard by the id passed
         controller.get(id, function(err, dashboard) {
             if(err) {
+                // If there is an error thrown log the error
                 console.log("Error: " + err);
+                // Return an error to the user
                 res.status(500).json({success:false, message: err});
                 return;
             } else {
@@ -74,20 +89,25 @@ router.route('/:dashboard_id')
     // Update a dashboard
     .put(function(req, res) {
 
+        // Get the id from the paremters passed
         var id = req.params.dashboard_id;
 
         console.log("Requested: PUT - /api/dashboard/" + id);
 
+        // store the dashboard object 
         var dashboard = req.body;
 
+        // Call the controller's update function
         controller.update(dashboard, function(err, updatedDashboard) {
 
             if(err) {
+                // If there is an error thrown log the error
                 console.log("Error: " + err);
+                // Return an error to the user
                 res.status(500).json({success:false, message: err});
                 return;
             } else {
-                // return the message
+                // return the result
                 res.json({success : true, message : "Dashboard Updated.", data : updatedDashboard});
             }
 
@@ -97,18 +117,22 @@ router.route('/:dashboard_id')
     // Delete a dashboard
     .delete(function(req, res) {
 
+        // Get the id from the paremters passed
         var dashboardId = req.params.dashboard_id;
 
         console.log("Requested: DELETE - /api/dashboard/" + dashboardId);
 
+        // Call the controller's delete function    
         controller.delete(dashboardId, function(err) {
 
             if(err) {
+                // If there is an error thrown log the error
                 console.log("Error: " + err);
+                // Return an error to the user
                 res.status(500).json({success:false, message: err});
                 return;
             } else {
-                // return the message
+                // return the result
                 res.json({success : true, message : "Dashboard removed."});
             }
 
