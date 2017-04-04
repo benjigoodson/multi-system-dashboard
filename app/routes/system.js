c9a0c7fc5ef8b4a8309a82b7d7ce52ac141016f4
Benjamin Goodson <Benjamin Goodson>
2017-04-04 11:58:27 +0100

    Added system route comments

--- app/routes/system.js
+++ app/routes/system.js
@@ -15,6 +15,7 @@ router.route('/')
     .get(function(req, res) {
         console.log("Requested: GET - /api/system");
 
+        // Call the controller's function to get all systems
         controller.getAll(function(err, systems) {
 
             if(err) {
@@ -24,6 +25,7 @@ router.route('/')
                 res.status(500).send({success:false, message: err});
                 return;
             } else {
+                // Return the results to the user
                 res.send({success:true, data : systems});
             }
 
@@ -35,6 +37,7 @@ router.route('/basic')
     .get(function(req, res) {
         console.log("Requested: GET - /api/system/basic");
 
+        // Call the controller's function to get all basic for the systems
         controller.getAllBasic(function(err, systems) {
 
             if(err) {
@@ -44,6 +47,7 @@ router.route('/basic')
                 res.status(500).send({success:false, message: err});
                 return;
             } else {
+                // Return the results to the user
                 res.send({success:true, data : systems});
             }
 
@@ -56,10 +60,13 @@ router.route('/')
     .post(function(req, res) {
         console.log("Requested: POST - /api/system");
 
+        // If a body is set
         if(req._body) {
 
+            // Store the new system
             var system = req.body;
 
+            // Call the controller's function to create a system
             controller.create(system, function(err, newSystem) {
 
                 if(err) {
@@ -69,6 +76,7 @@ router.route('/')
                     res.status(500).json({success:false, message: err.message});
                     return;
                 } else {
+                    // Return the result to the user
                     res.json({success:true, message: "System created.", data : newSystem});
                 }
 
@@ -82,10 +90,14 @@ router.route('/:system_id')
 
     // Get a unique system
     .get(function(req, res) {
-        console.log("Requested: GET - /api/system/" + req.params.system_id);
 
-        // Get system by the id passed
-        controller.get(req.params.system_id, function(err, system) {
+        // Store the system id
+        var id = req.params.system_id;
+
+        console.log("Requested: GET - /api/system/" + id);
+
+        // Call the controller function to get the system that matches a system id
+        controller.get(id, function(err, system) {
             if(err) {
                 // If there is an error thrown log the error
                 console.log("Error: " + err);
@@ -93,7 +105,7 @@ router.route('/:system_id')
                 res.status(500).send({success:false, message: err});
                 return;
             } else {
-                // return the system
+                // Return the result to the user
                 res.json(system);
             }
         });
@@ -102,13 +114,15 @@ router.route('/:system_id')
     // Update a system
     .put(function(req, res) {
 
+        // Store the system id
         var id = req.params.system_id;
 
         console.log("Requested: PUT - /api/system/" + id);
 
+        // Store the system
         var updatedSystem = req.body;
 
-        // Get system by the id passed
+        // Call the controller function to update a system
         controller.update(updatedSystem, function(err, response) {
             if(err) {
                 // If there is an error thrown log the error
@@ -117,7 +131,7 @@ router.route('/:system_id')
                 res.send(500, { success : false, error: err });
                 return;
             } else {
-                // return the message
+                // Return the result to the user
                 res.json(response);
             }
         });
@@ -126,10 +140,12 @@ router.route('/:system_id')
     // Delete a unique system
     .delete(function(req, res) {
 
+        // Store the system id
         var systemId = req.params.system_id;
 
         console.log("Requested: DELETE - /api/system/" + systemId);
 
+        // Call the controller function to delete a system
         controller.delete(systemId, function(err) {
 
             if(err) {
@@ -139,6 +155,7 @@ router.route('/:system_id')
                 res.status(500).send(err);
                 return;
             } else {
+                // Return the result to the user
                 res.json({success : true, message : "System removed."});
             }
 
