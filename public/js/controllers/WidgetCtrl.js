4f9dbc176695c9ae8986e607c0c87497fb0d1aa0
Benjamin Goodson <Benjamin Goodson>
2017-04-05 21:01:21 +0100

    Added comments

--- public/js/controllers/WidgetCtrl.js
+++ public/js/controllers/WidgetCtrl.js
@@ -12,6 +12,7 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 
 	var self = this;
 
+	// Stores a timer for creating a new widget
 	self.timer = undefined;
 
 	self.blacklist = [
@@ -34,19 +35,26 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 	$scope.edit = false;
 
 	this.createWidget = function(widgetData) {
+		// Create a new widget
 		WidgetService.create(widgetData).then(function(response) {
 			if(response.success == true) {
+				// Display a message to the user
 				notificationService.success(response.message);
+
+				// View the widget
 				$location.path("/widget/" + response.data._id);
 			} else {
+				// Display an error message
 				self.errorHandler("Unable to create widget:" + response.message);	
 			}
 		}, function(error) {
+			// Display an error message
 			self.errorHandler("Unable to create widget:" + error.message);
 		});
 	};
 
 	this.getWidget = function() {
+		// Get a widget for an id
 		WidgetService.getWidget(this.id).then(function(response) {
 
 			var widget = response.data;
@@ -60,8 +68,10 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 				// Generate chart for widget
 				ChartService.generateChartData(widget)
 					.then(function(widget) {
+						// If the widget is a bar chart
 						if(widget.graphType == "bar") {
 
+							// Set these options
 							widget.options = { 
 								scales: {
 									yAxes: [{
@@ -74,6 +84,7 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 							}; 
 
 						} else {
+							// Doon't set any custom options
 							widget.options = {};
                         }
 
@@ -84,9 +95,11 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 						$scope.widget.loading = false;
                         $scope.widget.error = errorMessage;
 
+						// Log the error
 						console.log("Unable to contact sever for widget: " + widget._id);
                     });
 			} catch(err) {
+				// Display an error message
 				self.errorHandler(err);
 			}
 
@@ -95,6 +108,7 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 	
 	this.setEdit = function() {
 
+		// Toggle the edit controls
 		if($scope.edit == true) {
 			$scope.edit = false;
 
@@ -112,9 +126,11 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 
 	this.saveWidget = function(widget) {
 
+		// Update a widget
 		WidgetService.update(widget).then(function(response) {
 
 			if(response.success == true) {
+				// Display a message to the user
 				notificationService.info(response.message);
 
 				// Now generate the graph
@@ -124,46 +140,62 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 					ChartService.generateChartData(widget)
 						.then(function(widget) {
 
+							// Hide the loading animation
 							widget.loading = false;
 							$scope.widget = widget;
 
 						}, function(errorMessage) {
+							// Hide the loading animation
 							$scope.widget.loading = false;
+
+							// Set an error message
 							$scope.widget.error = errorMessage;
 
+							// Log the error
 							console.log("Unable to contact sever for widget: " + widget._id);
 						});
 				} catch(err) {
+					// Display an error message
 					self.errorHandler(err);
 				}
 
 				self.setEdit();
 			} else {
+				// Display an error message
 				self.errorHandler(response.message)
 			}
 
 		}, function(err) {
+			// Display an error message
 			self.errorHandler(err.error);
 		});
 	}
 
 	this.deleteWidget = function(widgetId) {
 
+		// Set the modal values
 		var message = "Are you sure you wish to delete this widget, '" + $scope.widget.name + "' [" + widgetId + "]?";
 		var title = "Delete this widget?";
 
+		// Display the modal
 		ModalService.displayModal(message, title).result.then(function (modal_response) {
 			if(modal_response) {
+				// Delete the widget
 				WidgetService.delete(widgetId).then(function(response) {
 
 					if(response.success == true) {
+						// Display a message to the user
 						notificationService.success(response.message);
+
+						// View the home screen
 						$location.path("/");
 					} else {
+						// Display an error message
 						self.errorHandler("Unable to delete widget:" + response.message);
 					}
 
 				}, function(error) {
+					// Display an error message
 					self.errorHandler("Unable to delete widget.");
 				});
 			}
@@ -175,12 +207,14 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 	};
 
 	this.getWidgets = function() {
+		// Get all widgets
 		WidgetService.getAll().then(function(data) {
 			$scope.widgets = data;
 		});
 	};
 
 	this.getSystems = function() {
+		// Get all the basic system details
 		SystemService.getBasicSystems().then(function(response) {
 			if(response.success == true) {
 				$scope.systems = response.data;
@@ -193,6 +227,7 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 		$scope.endpoints = [];
 		$scope.widget.endpoint = "";
 
+		// If a system has been selected
 		if($scope.widget.system) {
 			var systemId = $scope.widget.system;
 
@@ -201,11 +236,13 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 
 			$scope.widget.fieldPath = [];
 
+			// Get endpoints
 			this.getEndpoints(systemId);
 		}
 	};
 
 	this.getEndpoints = function(systemId) {
+		// Get basic details for the endpoints matching a system id
 		EndpointService.getBasicEndpoints(systemId).then(function(response) {
 			if(response.success == true) {
 				$scope.endpoints = response.data;
@@ -214,6 +251,8 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 	};
 
 	this.endpointChanged = function() {
+
+		// If an endpoint is selected
 		if($scope.widget.endpoint) {
 
 			$scope.fields = [];
@@ -264,12 +303,13 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 		// If parameter has a value and form is valid
 		if($scope.widget.requestParam && $scope.widget.requestParam.length > 0 && this.getForm().$valid) {
 
-			// After 750 milli seconds make the request
+			// After 750 milliseconds make the request
 			self.timer = $interval(function() {
 
 				// Cancel it to stop it repeating
 				$interval.cancel(self.timer);
 
+				// Set the values
 				var method = $scope.widget.method;
 				var apiURL = $scope.widget.apiURL;
 				var requestParam = $scope.widget.requestParam;
@@ -341,8 +381,10 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 					// If selected dataset is an array
 					if(Array.isArray(self.searchableArray)) {
 
+						// Get the first object in the array
 						self.searchableObject = self.searchableArray[0];
 
+						// Traverse throguh the opbject to the field we want
 						self.searchableObject = self.traverseObject($scope.widget.fieldPath, self.searchableObject);
 						
 						// Populate fields
@@ -363,16 +405,21 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 
 		var fields = [];
 
+		// If the searchable object is just an object
 		if(searchableObject instanceof Object) {
 
+			// Get all of the properties that aren't in the blacklist
 			for(var field in searchableObject)
 			{	
 				if(!_.contains(self.blacklist, field)) {
+
+					// Add the field to the array
 					fields.push(field);
 				}
 			}
 		}
 
+		// Set the fields in the drop down
 		$scope.fields = fields;
 
 	};
@@ -461,6 +508,7 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 			// Display sample graph
 			ChartService.generateChartData($scope.widget).then(function(widgetData) {	
 
+				// Set the details to an example chart to use as an example
 				$scope.chart.graphType = widgetData.graphType;
 				$scope.chart.data = widgetData.data;
 				$scope.chart.labels = widgetData.labels;
@@ -470,21 +518,27 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 
 				$scope.chart.loading = false;
 				$scope.chart.error = errorMessage;
+				
+				// Log the error
 				console.log("Unable to contact sever for widget: " + $scope.widget._id);
 
 			});
 		} catch(err) {
 			$scope.chart.loading = false;
 			$scope.chart.error = err;
+
+			// Display an error message
 			self.errorHandler(err);
 		}
 	}
 
 	this.graphChanged = function() {
+		// If a graph type has been selected
 		if($scope.widget.graphType && $scope.widget.graphType.length > 1) {
 			// Display new graph
 			if($scope.widget.graphType == "pie") {
 
+				// Set example display labels
 				$scope.labels = [
 					"Dark Grey",
 					"Purple Color",
@@ -494,13 +548,17 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 				];
 
 				$scope.series = [];
+
+				// Set example data
 				$scope.data = [120, 50, 140, 180, 100];
 
 			} else if($scope.widget.graphType == "count") {
+				// Set example data
 				$scope.exampleCountField = "Colour";
 				$scope.exampleCount = "180";
 				$scope.exampleCountValue = "Green Color";
 			} else {
+				// Set example data
 				$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
 				$scope.series = ['Series A', 'Series B'];
 				$scope.data = [
@@ -515,6 +573,7 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 
 	this.setupCreatePage = function() {
 		
+		// Get all systems
 		this.getSystems();
 
 		$scope.widget = {};
@@ -527,8 +586,10 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 		$scope.datasets = [];
 		$scope.fields = [];
 		
+		// Set created date
 		$scope.widget.createdDate = moment().format('DD/MM/YYYY');
 
+		// Set the created by user
 		UserService.getCurrentUser().then(function(user) {
 			$scope.widget.createdBy = {id : user._id, forename : user.forename};
 		})
@@ -536,6 +597,8 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 	};
 
 	this.removeOneDatasetPath = function() {
+
+		// Rem,ove the last added dataset
 		$scope.widget.datasetPath.splice($scope.widget.datasetPath.length - 1, 1);
 
 		self.searchableArray = this.traverseObject($scope.widget.datasetPath, self.responseObject);
@@ -545,6 +608,8 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 	};
 
 	this.removeOneFieldPath = function() {
+
+		// Remvoe the last select field option
 		$scope.widget.fieldPath.splice($scope.widget.fieldPath.length - 1, 1);
 
 		self.searchableObject = this.traverseObject($scope.widget.fieldPath, self.searchableArray[0]);
@@ -554,6 +619,8 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 	};
 
 	this.clearFieldPath = function() {
+
+		// Completly clear the selected field options
 		$scope.widget.fieldPath = [];
 
 		self.searchableObject = self.searchableArray[0];
@@ -562,11 +629,14 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 	};
 
 	this.getForm = function() {
+		// Get the form displayed on screen
 		var scope = angular.element(document.getElementById("step-" + $("#wizard").smartWizard("currentStep") + "-form")).scope();
 
 		var form = scope["widgetForm" + $("#wizard").smartWizard("currentStep")];
 
+		// If a form has been found
 		if(form) {
+			// Return it to the calling method
 			return form;
 		}
 	};
@@ -578,12 +648,14 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 			return true;
 		}
 
+		// Get the form displayed on screen
 	  	var formId = "step-" + context.fromStep + "-form";
 
 	  	var scope = angular.element(document.getElementById(formId)).scope();
 
 		$scope.formValid = scope["widgetForm" + context.fromStep].$valid;
 
+		// Return if the form is valid
       	return $scope.formValid;
 	}
 
@@ -597,6 +669,7 @@ WidgetModule.controller('WidgetController', function($scope, $http, $routeParams
 	};
 
 	this.errorHandler = function(error) {
+		// Display an error message
         notificationService.error(error);
     };
 
