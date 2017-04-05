5216525e48c8f97f815004918148b7c5d126c7aa
Benjamin Goodson <Benjamin Goodson>
2017-04-05 19:20:59 +0100

    Added comments

--- public/js/services/ModalService.js
+++ public/js/services/ModalService.js
@@ -5,27 +5,32 @@ var MainModule = angular.module('MainModule');
 MainModule.factory('ModalService', function ($uibModal) {
 
     var self = {
-    
+
+        // Default the title and message to blank
         title : "",
         message : "",
 
         displayModal : function(message, title, parentSelector) {
 
+            // If no message is set use a default
             if(!message) {
                 self.message = "Are you sure you wish to delete this item?";
             } else {
                 self.message = message;
             }
 
+            // If no title is set use a default
             if(!title) {
                 self.title = "Please confirm."
             } else {
                 self.title = title;
             }
 
+            // Select an element in the right column to anchor the modal to
             var parentElem = parentSelector ? 
                 angular.element($document[0].querySelector('.right-col ' + parentSelector)) : undefined;
 
+            // Open the modal using the configuration options below
             var modalInstance = $uibModal.open({
                 animation: true,
                 ariaLabelledBy: 'modal-title',
@@ -42,6 +47,7 @@ MainModule.factory('ModalService', function ($uibModal) {
                 }
             });
 
+            // Return the instance of the modal
             return modalInstance;
         }
     };
