cb1a43b9f3f3034cdbab49bcab709b8bdb23a089
Benjamin Goodson <Benjamin Goodson>
2017-04-05 18:15:25 +0100

    Added comments

--- public/js/controllers/ModalCtrl.js
+++ public/js/controllers/ModalCtrl.js
@@ -1,35 +1,44 @@
 angular.module("MainModule").controller("ModalController", function (ModalService, $uibModalInstance) {
 
-    var $ctrl = this;
+    var self = this;
 
-    $ctrl.ok = function () {
+    // User clicked ok
+    self.ok = function () {
+        // Reset the title and message
         resetValues();
 
+        // Return true to the calling function
         $uibModalInstance.close(true);
     };
 
-    $ctrl.cancel = function () {
+    // User clicked cancel
+    self.cancel = function () {
+        // Reset the title and message
         resetValues();
 
+        // Return the reason for the cancel to the calling function
         $uibModalInstance.dismiss("cancelled");
     };
 
     var resetValues = function() {
+        // Reset the title and message on the service
         ModalService.title = "";
         ModalService.message = "";
 
-        $ctrl.title = "";
-        $ctrl.body= "";
+        // Reset the title and body
+        self.title = "";
+        self.body= "";
     } 
 
     var init = function() {
 
       // Set body and title based from service
-      $ctrl.title = ModalService.title;
-      $ctrl.body = ModalService.message;
+      self.title = ModalService.title;
+      self.body = ModalService.message;
 
     }
 
+    // Each time a new instance is made call the init function
     init();
 
 });
