3c330113d5b6de02e168861db6766b015dcb7c37
Benjamin Goodson <Benjamin Goodson>
2017-04-05 13:11:36 +0100

    Added comments and more error handling

--- app/controllers/endpoint.js
+++ app/controllers/endpoint.js
@@ -11,24 +11,33 @@ var controller = {};
 controller.getAll = function getAll (callback) {
     var countPromises = []
 
+    // Database query to get all endpoints
     Endpoint.find().lean().exec()
         .then(function endpointFind (endpoints) {
 
+            // For each endpoint
             endpoints.forEach(function (endpoint, i) {
 
+                // Get the system name and url
+                // Add the promise to an array
                 countPromises.push(controller.getSystemNameAndUrl(endpoint.parentSystem)
                     .then(function (system) {
+                        // Set the system name and url
                         endpoint.systemName = system.name;
                         endpoint.fullUrl = system.url + endpoint.url;
                 }))
             });
 
+            // When all promises are complete
             Promise.all(countPromises).then(function completedPromises () {
+                // Return the endpoints to the callback function
                 callback(undefined, endpoints);      
                 return;
             })
         })
         .catch(function errorHandler (error) {
+            // Catch any errors return error to the calling function
+            console.log("Error: " + error);
             callback(error);
         })
 
@@ -36,19 +45,26 @@ controller.getAll = function getAll (callback) {
 
 controller.get = function get (endpointId, callback) {
 
+    // Create a query for id's matching the passed id
     var query = {_id : endpointId};
 
+    // Database query to get an endpoint matchign the id
     Endpoint.findById(query).lean().exec().then(function endpointFindOne (endpoint) {
 
+        // Call fucntion to get the system name and url
         controller.getSystemNameAndUrl(endpoint.parentSystem).then(function (system) {
+            // Set the system name and url
             endpoint.systemName = system.name;
             endpoint.fullUrl = system.url + endpoint.url;
 
+            // Return the endpoint to the callback function
             callback(undefined, endpoint);
         })
 
     })
     .catch(function errorHandler (error) {
+        // Catch any errors return error to the calling function
+        console.log("Error: " + error);
         callback(error);
     })
 }
@@ -57,15 +73,20 @@ controller.getBasicEndpoints = function (systemId, callback) {
 
     var countPromises = []
 
+    // Database query to get all endpoints matching the parent system id
     Endpoint.find({parentSystem : systemId}).lean().exec().then(function (endpoints) {
 
         var basicEndpoints = [];
 
+        // For each endpoint
         endpoints.forEach(function (endpoint , i) {
 
+            // Get the system name and url
+            // Add the promises to an array
             countPromises.push(controller.getSystemNameAndUrl(endpoint.parentSystem)
                 .then(function (system) {
 
+                    // take the details and create an endpoint with basic details
                     basicEndpoints.push({
                         id : endpoint._id,
                         name : endpoint.name,
@@ -79,13 +100,17 @@ controller.getBasicEndpoints = function (systemId, callback) {
             }))
         })
 
+        // If all promises are complete
         Promise.all(countPromises).then(function completedPromises () {
+            // Return the basic details to the callback function
             callback(undefined, basicEndpoints);      
             return;
         })
 
     })
     .catch(function errorHandler (error) {
+        // Catch any errors return error to the calling function
+        console.log("Error: " + error);
         callback(error);
     })
 
@@ -98,14 +123,19 @@ controller.getSystemNameAndUrl = function(systemId) {
 
 controller.create = function create (newEndpoint, callback) {
 
-    var endpoint = Endpoint(newEndpoint);
+    // create a new endpoint that matches the Endpoint Schema
+    var endpoint = new Endpoint(newEndpoint);
 
+    // Database query to create a new endpoint
     endpoint.save(function(err, createdEndpoint) {
+
+        // If there is an error, return it
         if(err) {
             callback(err);
             return;
         }
 
+        // Return the created endpoint to the callback function
         callback(undefined, createdEndpoint);
 
     });
@@ -113,32 +143,45 @@ controller.create = function create (newEndpoint, callback) {
 
 controller.update = function update (endpoint, callback) {
         
+    // Create a query to match endpoints with the passed id
     var query = { "_id" : endpoint._id };
 
+    // Database query to find one endpoitn and update it
     Endpoint.findOneAndUpdate(query, endpoint, {new: true}, function(err, updatedEndpoint) {
 
+        // If there is an error, return it
         if(err) {
             callback(err);
             return;
         }
 
-        // return the endpoint
+        // return the endpoint to the callback method
         callback(undefined, updatedEndpoint);
 
     });
 }
 
-controller.delete = function create (endpointId, callback) {
+controller.delete = function delete (endpointId, callback) {
 
-    var method = this;
-
+    // Call widget controller method to remove delete widgets
+    // that use this endpoint
     WidgetController.deleteByEndpoint(endpointId, function (err) {
+
+        // If there is an error, return it
         if(err) {
             callback(err);
         } else {
+
+            // Database query to remove the endpoints
             Endpoint.remove({ _id : endpointId }, function(err) {
-                callback(err);
-                return;
+
+                 // If there is an error, return it
+                if(err) {
+                    self.callback(err);
+                    return;
+                }
+
+                callback();
             });
         }
     })
@@ -147,24 +190,35 @@ controller.delete = function create (endpointId, callback) {
 controller.deleteBySystem = function create (systemId, callback) {
 
     var self = this;
+
+    // Create a varible to hold the callback function
     self.callback = callback;
 
+    // Database query to return endpoints matching the system id
     Endpoint.find({ parentSystem : systemId }).lean().exec()
         .then(function (endpoints) {
-
+        
+        // For each ednpoint
         endpoints.forEach(function (endpoint, i) {
 
+            // Call the controlelrs delete method to remove them from the database
+            // This also removes any other entities that sue it
             self.delete(endpoint._id, function (err) {
+
+                // If there is an error, return it
                 if(err) {
                     self.callback(err);
                     return;
                 }
+
+                self.callback();
             })
         });
-
-        self.callback();   
+  
     })
     .catch(function errorHandler (error) {
+        // Catch any errors return error to the calling function
+        console.log("Error: " + error);
         self.callback(error);
         return;
     })
